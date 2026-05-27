8d2b4a2ff8261f3c13b6987c0220bf83909219c3
flamboyant28 <28enes28@gmail.com>
2026-05-27 19:52:37 +0300

    Update hava.py

--- hava.py
+++ hava.py
@@ -299,8 +299,8 @@ with st.sidebar:
     forecast_days = st.slider("Tahmin Günü", 1, 16, 16)
     st.divider()
     st.markdown("**Hızlı Şehirler**")
-    for s in ["İstanbul","Ankara","İzmir","Antalya","Trabzon",
-              "London","New York","Paris","Tokyo","Dubai","Yakutsk","Reykjavik"]:
+    for s in ["Üsküdar","Çankaya","Seyhan","Görele",
+              "Basra","Sukkur","He'an","Tokyo","Helsinki","Yakutsk","Reykjavik","Mykines"]:
         if st.button(s, key=f"hz_{s}", use_container_width=True):
             st.session_state.ara1 = s
             st.session_state.tetik1 = True
