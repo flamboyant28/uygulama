ddef067bc11aa646cb96eb08740e583234fed56a
flamboyant28 <28enes28@gmail.com>
2026-05-31 09:58:41 +0300

    Update yakithes2.py

--- yakithes2.py
+++ yakithes2.py
@@ -1193,9 +1193,9 @@ with tab_ozet:
             sc1, sc2, sc3, sc4 = st.columns(4)
             il0, ilce0 = plan_rows[0]["Konum"].split(" / ")
             lat0, lon0 = koordinat(il0.strip(), ilce0.strip())
-            sc1.link_button("🟢 ZES",      "https://www.zes.net/sarj-istasyonlari",    use_container_width=True)
-            sc2.link_button("🔵 Trugo",    "https://trugo.com.tr/sarj-noktalari",      use_container_width=True)
-            sc3.link_button("🟠 Eşarj",   "https://www.esarj.com/sarj-istasyonlari",  use_container_width=True)
+            sc1.link_button("🟢 ZES",      "https://zes.net/tr/sarj-istasyonlari",    use_container_width=True)
+            sc2.link_button("🔵 Trugo",    "https://trugo.com.tr/network",      use_container_width=True)
+            sc3.link_button("🟠 Eşarj",   "https://esarj.com/esarj-noktalari",  use_container_width=True)
             sc4.link_button("⚡ PlugShare",
                 f"https://www.plugshare.com/?latitude={lat0}&longitude={lon0}&spanLat=0.5&spanLng=0.5",
                 use_container_width=True)
