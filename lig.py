121377b7e4e8ec4c4e3abe352f8a2a0d14a84896
flamboyant28 <28enes28@gmail.com>
2026-09-24 18:33:44 +0300

    Update lig.py

--- lig.py
+++ lig.py
@@ -1224,7 +1224,7 @@ with tab5:
                     html_g += f'<th class="{"left" if h=="Takım" else ""}">{h}</th>'
                 html_g += "</tr></thead><tbody>"
                 for i, r in enumerate(sorted_att):
-                    bg = "#1a2340" if i % 2 == 0 else ""
+                    bg = "#bcbcbc" if i % 2 == 0 else ""
                     html_g += (
                         f'<tr style="background:{bg}">'
                         f'<td class="left">{r["Takım"]}</td>'
